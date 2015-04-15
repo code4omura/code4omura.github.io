d97ff5941eead6a3a57f182567f76e47a9680a94
Davenakao <official@mt-design.info>
2015-04-15 14:55:49 +0900

    icon data changed2

--- js/setting.js
+++ js/setting.js
@@ -19,19 +19,33 @@ OpenSpending.Styles.Cofog = {
 
   /* omura cofog */
   '201501': { icon: 'icons/legislative.svg', color: '#C75746', bcolor: '#935B3B' },
+  '20150101': { icon: 'icons/legislative.svg', color: '#C75746', bcolor: '#935B3B' },
   '201502': { icon: 'icons/admin.svg', color: '#C75746', bcolor: '#935B3B' },
+  '20150201': { icon: 'icons/admin.svg', color: '#C75746', bcolor: '#935B3B' },
   '201503': { icon: 'icons/family.svg', color: '#C75746', bcolor: '#0AB971' },
+  '20150301': { icon: 'icons/family.svg', color: '#C75746', bcolor: '#0AB971' },
   '201504': { icon: 'icons/health.svg', color: '#C75746', bcolor: '#0AB971' },
+  '20150401': { icon: 'icons/health.svg', color: '#C75746', bcolor: '#0AB971' },
   '201505': { icon: 'icons/labour.svg', color: '#C75746', bcolor: '#0AB971' },
+  '20150501': { icon: 'icons/labour.svg', color: '#C75746', bcolor: '#0AB971' },
   '201506': { icon: 'icons/farms.svg', color: '#C75746', bcolor: '#4E6D00' },
+  '20150601': { icon: 'icons/farms.svg', color: '#C75746', bcolor: '#4E6D00' },
   '201507': { icon: 'icons/misc-services.svg', color: '#C75746', bcolor: '#4E6D00' },
+  '20150701': { icon: 'icons/misc-services.svg', color: '#C75746', bcolor: '#4E6D00' },
   '201508': { icon: 'icons/manufactoring-construction.svg', color: '#C75746', bcolor: '#4E6D00' },
+  '20150801': { icon: 'icons/manufactoring-construction.svg', color: '#C75746', bcolor: '#4E6D00' },
   '201509': { icon: 'icons/fire-brigade.svg', color: '#C75746', bcolor: '#790586' }
+  '20150901': { icon: 'icons/fire-brigade.svg', color: '#C75746', bcolor: '#790586' }
   '201510': { icon: 'icons/pre-school.svg', color: '#C75746', bcolor: '#790586' }
+  '20151001': { icon: 'icons/pre-school.svg', color: '#C75746', bcolor: '#790586' }
   '201511': { icon: 'icons/our-streets.svg', color: '#C75746', bcolor: '#790586' }
+  '20151101': { icon: 'icons/our-streets.svg', color: '#C75746', bcolor: '#790586' }
   '201512': { icon: 'icons/money.svg', color: '#C75746', bcolor: '#790586' }
+  '20151201': { icon: 'icons/money.svg', color: '#C75746', bcolor: '#790586' }
   '201513': { icon: 'icons/research.svg', color: '#C75746', bcolor: '#790586' }
+  '20151301': { icon: 'icons/research.svg', color: '#C75746', bcolor: '#790586' }
   '201514': { icon: 'icons/unknown.svg', color: '#C75746', bcolor: '#790586' }
+  '20151401': { icon: 'icons/unknown.svg', color: '#C75746', bcolor: '#790586' }
 };
 
 
